163bbfa43daeda96d212a52d18374145e9ad15cb
KhrTim <42896525+KhrTim@users.noreply.github.com>
2021-03-30 12:24:58 +0300

    Update demonstration.py

--- python/demonstration.py
+++ python/demonstration.py
@@ -2,6 +2,6 @@ from ascii_artist import *
 
 if __name__ == "__main__":
 
-    font = FontSetup()
+    font = FontSetup() #by default loads Ubuntu font. If you want to use any other one, you should specify path to .ttf file
     artist = Artist(font)
     artist.create_art("train.jpg", "ascii_train.jpg")
